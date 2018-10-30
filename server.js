f9bc8d1b21f59e1109a9af0960eeed8e39d1e86e
tdshivendran <tdshivendran@gmail.com>
2018-10-30 16:15:50 -0600

    Added server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+var cmd=require('node-cmd');
+
+const app = express();
+
+const port = 3005;
+
+var process=cmd.get('node');
+console.log(process.pid);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.get('/hello', (req, res) => {
+    res.send("Hello From Express");
+});
+
+app.post('/send', (req, res) => {
+
+    cmd.get(
+        req.body.post,
+        function(err, data, stderr){
+            console.log(data)
+        }
+    );
+
+    res.send(
+        `This is what you sent me: ${req.body.post}`,
+    );
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            response: '',
+            post: '',
+            responseToPost: '',
+        };
+    }
 
+    async componentDidMount() {
+        const response =  await fetch('/hello');
+        const body = await response.text();
+        console.log(body);
+        this.setState({ responseToPost: body });
+    }
+
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ post: this.state.post }),
+        });
+        const body = await response.text();
+        this.setState({ responseToPost: body });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <p>{this.state.response}</p>
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                        <strong>Post to Server:</strong>
+                    </p>
+                    <input
+                        type="text"
+                        value={this.state.post}
+                        onChange={e => this.setState({ post: e.target.value })}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+                <p>{this.state.responseToPost}</p>
+            </div>
+        );
+    }
+}
 export default App;
